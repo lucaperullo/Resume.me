c91aad6ade1cb78dcdbd8483f16842822a4ccd53
Luca <lucaperullo@outlook.it>
2023-01-13 20:07:20 +0100

    asd

--- environment.d.ts
+++ environment.d.ts
@@ -1,11 +1,37 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      GITHUB_AUTH_TOKEN: string;
-      NODE_ENV: 'development' | 'production';
-      PORT?: string;
-      PWD: string;
+      MONGO_URI: string;
+
+NODE_ENV:string;
+
+PORT:string;
+
+JWT_SECRET:string;
+
+REFRESH_TOKEN_SECRET:string;
+
+CLOUDINARY_CLOUD_NAME:string;
+CLOUDINARY_API_KEY:string;
+CLOUDINARY_API_SECRET:string
     }
+  }
+}
+declare var process : {
+  env: {
+    MONGO_URI: string;
+
+    NODE_ENV:string;
+    
+    PORT:string;
+    
+    JWT_SECRET:string;
+    
+    REFRESH_TOKEN_SECRET:string;
+    
+    CLOUDINARY_CLOUD_NAME:string;
+    CLOUDINARY_API_KEY:string;
+    CLOUDINARY_API_SECRET:string
   }
 }
 export {}
